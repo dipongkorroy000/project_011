0905393fe2ae439e763ef7dfa19639ce6c7f91b1
dipongkorroy000 <dipongkorroy000@gmail.com>
2025-06-22 20:15:03 +0600

    finally fixed page scrolling task

--- src/pages/home/AllPhoto.jsx
+++ src/pages/home/AllPhoto.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Cart from "./Cart";
 
 const AllPhoto = () => {
+  const [page, setPage] = useState(0);
   const [dataLength, setDataLength] = useState(0);
   const [moreBtn, setMoreBtn] = useState(false);
   const [photos, setPhotos] = useState([]);
@@ -15,7 +16,6 @@ const AllPhoto = () => {
         setDataLength(data.length);
         let countData = [];
         for (let n = startNum; n <= endNum; n++) {
-          console.log(n);
           const find = data.find((i) => i.id === n);
           countData.push(find);
         }
@@ -31,6 +31,7 @@ const AllPhoto = () => {
 
   const handlePrevious = () => {
     if (startNum > 1) {
+      setPage(page - 1);
       setStartNum(startNum - 8);
       setEndNum(endNum - 8);
     }
@@ -38,6 +39,7 @@ const AllPhoto = () => {
 
   const handleNext = () => {
     if (endNum <= dataLength) {
+      setPage(page + 1);
       setStartNum(startNum + 8);
       setEndNum(endNum + 8);
     }
@@ -45,28 +47,33 @@ const AllPhoto = () => {
 
   return (
     <div className="flex flex-col gap-5">
+      {/* data */}
       <div className="grid grid-cols-4 max-2xl:grid-cols-3 max-lg:grid-cols-2 max-md:grid-cols-1 gap-8">
         {photos.map((n, index) => (
           <Cart n={n} key={index}></Cart>
         ))}
       </div>
+
       <div className="flex justify-center border border-gray-600 p-2 rounded-xl">
+        {/* seemore button  */}
         <button
           onClick={handleSeeMore}
           className={`btn btn-primary ${moreBtn && "hidden"}`}
         >
           See More
         </button>
-        <div className={`join grid grid-cols-2 ${moreBtn ? "" : "hidden"}`}>
-          <button
-            onClick={handlePrevious}
-            className="join-item btn btn-outline"
-          >
-            Previous page
-          </button>
-          <button onClick={handleNext} className="join-item btn btn-outline">
-            Next
-          </button>
+
+        {/* page scroll  */}
+        <div className={`${moreBtn ? "" : "hidden"}`}>
+          <div className="join">
+            <button onClick={handlePrevious} className="join-item btn">
+              «
+            </button>
+            <button className="join-item btn">Page {page}</button>
+            <button onClick={handleNext} className="join-item btn">
+              »
+            </button>
+          </div>
         </div>
       </div>
     </div>
